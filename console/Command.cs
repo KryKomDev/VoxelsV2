b6ec3e865fce087403a99b7c330711d29d34a01f
KryKomDev <krystof.komzak@gmail.com>
2024-05-14 17:34:59 +0200

    restructured command structure

--- console/Command.cs
+++ console/Command.cs
@@ -3,19 +3,43 @@
 // by KryKomDev
 //
 
+using System.Linq.Expressions;
+
 namespace VoxelsCoreSharp.console;
 
+/// <summary>
+/// voxels shell command parser
+/// </summary>
 public class CommandParser {
-    public static Command[] commands = [
-        new Command("world", [], () => Console.WriteLine($"world file location: {Global.WORLD_MANAGER.WorldFilePath}")),
-        new Command("world save", [], () => {})
     
+    public static Program[] commands = [
+        new Program("world", [
+            new Command("info", [], () => Console.WriteLine(Global.WORLD_MANAGER.WorldFilePath)),
+            new Command("load", [(int)ArgumentType.STRING], (string path) => Console.WriteLine(path) ),
+            new Command("save", [], () => Console.WriteLine("closing"))
+        ]),
+        new Program("place", [
+            new Command()
+        ])
     ];
     
 } 
 
-public struct Command(string name, object[] args, Action func) {
-    public string name = name;
-    public object[] args = args;
-    public Action func = func;
+public struct Program(string programName, Command[] commands) {
+    public string programName = programName;
+    public Command[] commands = commands;
+}
+
+public struct Command(string commandName, int[] args, LambdaExpression functionality) {
+    public string commandName = commandName;
+    public int[] args = args;
+    public LambdaExpression functionality = functionality;
+}
+
+public enum ArgumentType {
+    VOID = 0,
+    STRING = 1,
+    INT = 2,
+    LONG = 3,
+    BOOL = 4
 }
